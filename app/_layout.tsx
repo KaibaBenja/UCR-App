b860b8e1aad52fd9c0d0e8e8b7a8c2da3564582c
Benjamin Sanchez Morales <sanchezmoralesbenjamin10@gmail.com>
2025-02-14 12:05:51 -0300

    add: router

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function layout() {
+  return (
+    <View className="flex-1 bg-blue-600">
+      <Text className="font-bold">Hola</Text>
+    </View>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: [
     "app/index.js",
+    "./app/**/*.{js,jsx,ts,tsx}",
     "./App.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
